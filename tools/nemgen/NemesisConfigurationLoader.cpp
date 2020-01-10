1119370abf16a496eb5bee5edd839c9e72e16205
Greg S <greg@evias.be>
2020-01-10 13:48:33 +0100

    fix const

--- tools/nemgen/NemesisConfigurationLoader.cpp
+++ tools/nemgen/NemesisConfigurationLoader.cpp
@@ -106,11 +106,12 @@ namespace catapult { namespace tools { namespace nemgen {
 			for (const auto& pair : config.SignedTransactionEntries) {
 				const auto& signerPublicKey = pair.first;
 				const auto& transactionEntry = pair.second;
+				auto transactionPayloads = transactionEntry.GetPayloads();
 
 				CATAPULT_LOG(debug) << " - Signer: " << signerPublicKey;
 
 				auto i = 0u;
-				for (const auto& payload : transactionEntry.GetPayloads()) {
+				for (const auto& payload : transactionPayloads) {
 					CATAPULT_LOG(debug) << " - Payload (" << i << "): " << payload.ToHex();
 					++i;
 				}
